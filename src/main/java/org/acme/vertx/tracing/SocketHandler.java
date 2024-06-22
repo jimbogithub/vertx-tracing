87bef7fe97cb75b7f03591a5c2677647f063088e
James Olsen <james@coinzoom.com>
2024-06-22 12:06:50 +1200

    Solution.

--- src/main/java/org/acme/vertx/tracing/SocketHandler.java
+++ src/main/java/org/acme/vertx/tracing/SocketHandler.java
@@ -1,8 +1,12 @@
 package org.acme.vertx.tracing;
 
+import static io.quarkus.vertx.core.runtime.context.VertxContextSafetyToggle.setCurrentContextSafe;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.smallrye.common.vertx.VertxContext;
+import io.vertx.core.Context;
 import io.vertx.core.Handler;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.net.NetSocket;
@@ -16,7 +20,7 @@ class SocketHandler implements Handler<Buffer>, Responder {
 
 	SocketHandler(NetSocket socket, MessageHandler messageHandler) {
 		this.socket = socket;
-		this.messageHandler = messageHandler;
+		this.messageHandler = enforceDuplicatedContext(messageHandler);
 	}
 
 	@Override
@@ -31,6 +35,28 @@ class SocketHandler implements Handler<Buffer>, Responder {
 		LOG.info("Responding: {}", response);
 		Buffer output = Buffer.buffer(response);
 		socket.write(output);
+	}
+
+	/* See https://github.com/quarkusio/quarkus/discussions/41346#discussioncomment-9836900 */
+	private static MessageHandler enforceDuplicatedContext(MessageHandler delegate) {
+		return new MessageHandler() {
+			@Override
+			public void handle(String message) {
+				if (!VertxContext.isOnDuplicatedContext()) {
+					Context context = VertxContext.createNewDuplicatedContext();
+					context.runOnContext(new Handler<Void>() {
+						@Override
+						public void handle(Void x) {
+							setCurrentContextSafe(true);
+							delegate.handle(message);
+						}
+					});
+				} else {
+					setCurrentContextSafe(true);
+					delegate.handle(message);
+				}
+			}
+		};
 	}
 
 }
